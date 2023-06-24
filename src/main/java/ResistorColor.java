3c34d7f911de1bcde3ad1c56d849ae2666993d89
anushka7701 <anushka03008@gmail.com>
2023-06-24 22:20:33 +0530

    initial commit

---
+++ src/main/java/ResistorColor.java
@@ -0,0 +1,44 @@
+class ResistorColor {
+    int colorCode(String color) {
+        int ans=-1;
+        switch (color)
+        {
+            case "black":
+                ans=0;
+                break;
+            case "brown":
+                ans=1;
+                break;
+            case "red":
+                ans=2;
+                break;
+            case "orange":
+                ans=3;
+                break;
+            case "yellow":
+                ans=4;
+                break;
+            case "green":
+                ans=5;
+                break;
+            case "blue":
+                ans=6;
+                break;
+            case "violet":
+                ans=7;
+                break;
+            case "grey":
+                ans=8;
+                break;
+            case "white":
+                ans=9;
+                break;
+        }
+        return ans;
+    }
+
+    String[] colors() {
+        String ans[]={"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+        return ans;
+    }
+}
